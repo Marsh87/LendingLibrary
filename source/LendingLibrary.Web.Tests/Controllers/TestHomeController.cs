25a3b859208ca30849404f15cbfe5311e08c2f64
unknown <Mahesh@Mahesh.chilli.local>
2016-01-15 16:33:28 +0200

    Added base solution with first tests

---
+++ source/LendingLibrary.Web.Tests/Controllers/TestHomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LendingLibrary.Web.Controllers;
+using NUnit.Framework;
+
+namespace LendingLibrary.Web.Tests
+{
+    [TestFixture]
+    public class TestHomeController
+    {
+        [Test]
+        public void Construct_ShouldNotThrow()
+        {
+            //---------------Set up test pack-------------------
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Assert.DoesNotThrow(()=>Create());
+            //---------------Test Result -----------------------
+                  
+        }
+
+        private static HomeController Create()
+        {
+            return new HomeController();
+        }
+    }
+}
