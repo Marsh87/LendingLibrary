5a0d100a6eb2efc5e3ad515b517870e90781361d
unknown <Mahesh@Mahesh.chilli.local>
2016-01-22 17:13:53 +0200

    Added Person Entity with tests

---
+++ source/LendingLibrary.Domain.Tests/Models/TestPerson.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PeanutButter.TestUtils.Generic;
+
+namespace LendingLibrary.Domain.Tests.Models
+{
+    [TestFixture]
+    public class TestPerson
+    {
+        [Test]
+        public void Construct_ShouldNotThow()
+        {
+            //---------------Set up test pack-------------------
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Assert.DoesNotThrow(()=>Create());
+            //---------------Test Result -----------------------
+        }
+
+        [TestCase("PersonId",typeof(int))]
+        [TestCase("FirstName",typeof(string))]
+        [TestCase("PhoneNumber",typeof(string))]
+        [TestCase("Email",typeof(string))]
+        [TestCase("Photos",typeof(byte[]))]
+        public void Type_ShouldHaveProperty_(string propertyName,Type propertyType)
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof (Person);
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            sut.ShouldHaveProperty(propertyName,propertyType);
+            //---------------Test Result -----------------------
+        }
+
+        private static void Create()
+        {
+            var person = new Person();
+        }
+    }
+}
---
+++ source/LendingLibrary.Domain/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace LendingLibrary.Domain.Tests.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set;}
+        public string PhoneNumber { get; set;}
+        public string Email { get; set;}
+        public byte[] Photos { get; set; }
+    }
+}
